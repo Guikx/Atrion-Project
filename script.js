ea6ed2c2bb24dbef5cbee648e946397ce7cfdc19
Guik's <121995320+Guikx@users.noreply.github.com>
2023-12-20 22:33:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+ link.addEventListener('click', event => {
+    event.preventDefault();
+    const targetSection = document.querySelector(event.target.getAttribute('href'));
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+ });
+});
+
